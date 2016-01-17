f65ccf8b80e0d01129094bcbd99f227e142f049d
zhangyuhao <zhangyuhao1158@sina.com>
2016-01-17 18:22:19 +0800

    删除以前的方法前的备注

--- YFTextField/ViewController.m
+++ YFTextField/ViewController.m
@@ -25,9 +25,8 @@
 {
     [super viewDidLoad];
     
-    //leftView只有文字  下面带细线
-    [self.textField setupTextFieldWithLeftViewText:@"百度一下："  textViewLeftViewSpacing:10 andFineLine:YES];
-    [self.textField setPlaceholder:@"百度一下，你就知道"];
+    
+    
     
     //leftView只有文字  下面不带细线
 //    [self.textField1 setYFTextFieldBorderStyle:YFTextFieldBorderStyleUnderLine];
@@ -42,15 +41,20 @@
     [self.textField1 setYFTextFieldWithLeftViewImage:[UIImage imageNamed:@"pic_user"] spacing:10 imageWH:CGSizeMake(21, 21)];
     
     
-    //leftView带有图片
-    [self.textField2 setupTextFieldWithLeftViewImage:[UIImage imageNamed:@"pic_user"] textViewLeftViewSpacing:10 andFineLine:YES];
+    
+    
     
     //没有 leftView
-    [self.textField3 setupTextFieldWithFineLine:YES];
+    
+    [self.textField3 setYFTextFieldBorderStyle:YFTextFieldBorderStyleUnderLine];
+    [self.textField3 setYFTextFieldWithBorderLineColor:[UIColor lightGrayColor] andWidth:1];
+    [self.textField3 setYFTextFieldWithPlaceholderText:@"百度一下" textFont:[UIFont systemFontOfSize:13] andTextColor:[UIColor lightGrayColor]];
     
     //自己初始化的textfield
     YFTextField *yfTextField = [[YFTextField alloc]initWithFrame:CGRectMake(20, 90, 200, 30)];
-    [yfTextField setupTextFieldWithLeftViewText:@"百度" textViewLeftViewSpacing:10 andFineLine:YES];
+    [yfTextField setYFTextFieldBorderStyle:YFTextFieldBorderStyleUnderLine];
+    [yfTextField setYFTextFieldWithLeftViewText:@"百度：" spacing:10 textFont:[UIFont systemFontOfSize:14] andTextColor:[UIColor lightGrayColor]];
+    [yfTextField setYFTextFieldWithBorderLineColor:[UIColor lightGrayColor] andWidth:1];
     [self.view addSubview:yfTextField];
     
 }
